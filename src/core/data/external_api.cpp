d1fcd0d0e681bf6a4385b87c01a9b7f4f0db7e32
Louis Sugy <louis.sugy@gmail.com>
2018-11-09 09:57:43 +0100

    Implemented building recognition via OpenStreetMap API + improved compilation

--- src/core/data/external_api.cpp
+++ src/core/data/external_api.cpp
@@ -1,8 +1,50 @@
 #include "external_api.h"
 #include <iostream>
+#define FMT_HEADER_ONLY
+#include "fmt/format.h"
+#include "httplib.h"
+#include "json.hpp"
 
 using namespace std;
+using json = nlohmann::json;
 
 void data::api_query() {
 	cout << "The external API says hi" << endl;
+}
+
+
+data::Building* data::get_building(float lat, float lon, float radius)
+{
+	// Compose the Overpass query
+	string query = fmt::format(
+		"[out:json][timeout:10];"
+		" way(around:{2},{0},{1})['building'~'.*'];"
+		" out tags;",
+		lat, lon, radius
+	);
+
+	string api_url = "lz4.overpass-api.de";
+	httplib::Client httpclient(api_url.c_str(), 80);
+	auto res = httpclient.Post("/api/interpreter", query, "text/plain");
+
+	if(res == nullptr) {
+		cout << "API query failed on " << api_url << ":" << endl << query << endl;
+		return nullptr;
+	}
+
+	// cout << res->body << endl;
+
+	json json_data = json::parse(res->body);
+
+	Building* building = new Building;
+	if(json_data["elements"].size() >= 1) {
+		building->name = json_data["elements"][0]["tags"]["name"].get<std::string>();
+		building->type = json_data["elements"][0]["tags"]["building"].get<std::string>();
+	}
+	else {
+		building->name = "";
+		building->type = "unknown";
+	}
+	
+	return building;
 }
--- src/core/include/external_api.h
+++ src/core/include/external_api.h
@@ -1,6 +1,16 @@
 #pragma once
+#include <string>
+
+using namespace std;
 
 namespace data
 {
-	void api_query();
+	typedef struct {
+		string name;
+		string type;
+	} Building;
+
+	void api_query(); // placeholder
+
+	Building* get_building(float lat, float lon, float radius = 5.0f);
 }
