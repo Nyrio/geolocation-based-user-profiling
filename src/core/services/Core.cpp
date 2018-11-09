296575ac9c3b7649f5e38b1b810f0f7fa4d6a159
PtiLuky <luky.herbin@free.fr>
2018-11-09 11:04:17 +0100

    Read params in Core

--- src/core/services/Core.cpp
+++ src/core/services/Core.cpp
@@ -3,10 +3,13 @@
 
 #include "LocPoint.h"
 #include "rawdata.h"
+#include "workflow.h"
 
 services::Core::Core()
 {
 	std::cout << "Core created" << std::endl;
+	
+	data::WorkflowParam w = data::loadParam();
 }
 
 
