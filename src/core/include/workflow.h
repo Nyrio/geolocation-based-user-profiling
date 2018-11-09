e79f4e89ad83b290f62a5b784070d0c7e436c88b
PtiLuky <luky.herbin@free.fr>
2018-11-09 10:58:24 +0100

    workflow file read to WorflowParam struct

--- src/core/include/workflow.h
+++ src/core/include/workflow.h
@@ -5,7 +5,9 @@
 namespace data{
     struct WorkflowParam {
         std::string param1;
+        int param2;
     };
     WorkflowParam loadParam(std::string file = "./config.txt");
-    void parseWorkflow(WorkflowParam w, std::string s);
+    void parseWorkflow(WorkflowParam &w, std::string s);
+    void resetWorkflow(WorkflowParam &w);
 }
