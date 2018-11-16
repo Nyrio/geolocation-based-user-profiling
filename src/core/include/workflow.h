c08ca3eef115fe398e018aa03ee3a947967e2551
capellev <capellev.info@gmail.com>
2018-11-16 09:32:27 +0100

    Add eps & minPts parameters

--- src/core/include/workflow.h
+++ src/core/include/workflow.h
@@ -6,6 +6,8 @@ namespace data{
     struct WorkflowParam {
         std::string param1;
         int param2;
+        double eps;
+        int minPts;
     };
     WorkflowParam loadParam(std::string file = "./config.txt");
     void parseWorkflow(WorkflowParam &w, std::string s);
